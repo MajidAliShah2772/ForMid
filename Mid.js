25ecac27a21189f7f54b3b967551bd8c9b0dd7b1
MajidShah2772 <majidalishahbscsf22@iba-suk.edu.pk>
2025-03-24 22:37:38 +0500

    File Added

---
+++ Mid.js
@@ -0,0 +1,16 @@
+import react from 'react'
+
+function Mid(){
+   
+    return(
+        
+            <div>
+              <h1>
+                hello world
+              </h1>
+             </div>
+        
+        )
+}
+
+export default Mid
